5a4b072b0b59830fddba9c71e95bdeb54b96eab1
MalMWare <malkelleymartin@gmail.com>
2023-02-11 22:24:27 -0800

    created a paces view page with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Gorilla Sushi',
+        city: 'Las Vegas',
+        state: 'NV',
+        cuisines: 'Sushi, Japanese Food, Buffet/AYCE',
+        pic: 'https://placekitten.com/250/250'
+    }, {
+        name: 'Black Tap',
+        city: 'Las Vegas',
+        state: 'NV',
+        cuisines: 'American, Burgers, Shakes',
+        pic: 'https://placekitten.com/250/250'
+    }]
+    res.render('places/index', { places })
 })
 
 module.exports = router
