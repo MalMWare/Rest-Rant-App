ce7a49236f3a933a32220bea363247011d04c990
MalMWare <malkelleymartin@gmail.com>
2023-02-14 03:13:07 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,7 @@
 const router = require('express').Router()
+const places = require("../models/places.js")
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Gorilla Sushi',
-        city: 'Las Vegas',
-        state: 'NV',
-        cuisines: 'Sushi, Japanese Food, Buffet/AYCE',
-        pic: '/images/smallSushiDinner.jpg',
-        id: 'sushi-dinner',
-        credit: "https://unsplash.com/de/@metzograf?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-        source: "https://unsplash.com/photos/12IsagncQ7o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-        creditName: 'Florian Metzner',
-        sourceName: 'Unsplash'
-    }, {
-        name: 'Black Tap',
-        city: 'Las Vegas',
-        state: 'NV',
-        cuisines: 'American, Burgers, Shakes',
-        pic: '/images/smallIcecream.jpg',
-        id: 'icecream-shake',
-        credit: "https://unsplash.com/@m_usm04?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-        source: "https://unsplash.com/photos/OSpvTwWvjmU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-        creditName: 'Muhammad Usman',
-        sourceName: 'Unsplash'
-  
-    }]
     res.render('places/index', { places })
 })
 
@@ -33,8 +10,17 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        req.body.pic = "https://placekitten.com/400/500"
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anywhere'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,23 @@
+module.exports = [{
+    name: 'Gorilla Sushi',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Sushi, Japanese Food, Buffet/AYCE',
+    pic: '/images/smallSushiDinner.jpg',
+    id: 'sushi-dinner',
+    credit: "https://unsplash.com/de/@metzograf?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    source: "https://unsplash.com/photos/12IsagncQ7o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    creditName: 'Florian Metzner',
+    sourceName: 'Unsplash'
+}, {
+    name: 'Black Tap',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'American, Burgers, Shakes',
+    pic: '/images/smallIcecream.jpg',
+    id: 'icecream-shake',
+    credit: "https://unsplash.com/@m_usm04?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    source: "https://unsplash.com/photos/OSpvTwWvjmU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    creditName: 'Muhammad Usman',
+    sourceName: 'Unsplash'
+}]
