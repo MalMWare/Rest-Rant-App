55964d3d35a00e6424603ef6325808979067ac01
MalMWare <malkelleymartin@gmail.com>
2023-02-18 02:58:24 -0800

    Delete route created and functional

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,8 @@
 const router = require('express').Router()
 const places = require("../models/places.js")
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    } 
-    else {
-        res,render('places/show', {place: places[id]})
-    }
-})
-
+//create
 router.post('/', (req, res) => {
     if (!req.body.pic) {
         req.body.pic = "https://placekitten.com/400/500"
@@ -35,5 +16,49 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
 })
+
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+//new
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id], id })
+    }
+})
+
+router.delete("/:id", (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        //res.send('STUB DELETE places/:id')
+        res.redirect('/places')
+    }
+})
+  
+// router.get('/:id', (req, res) => {
+//     (places[req.params.id])
+//       ? res.render('places/show', { place: places[req.params.id], id: req.params.id })
+//       : res.status(404).render('Error404')
+// })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,30 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show ({place, id}) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>
+                    { place.name }
+                </h1>
+                <div className='imgContainer'>
+                    <img src={place.pic} alt={place.name} id={place.id}/>
+                    <p>Photo by <a href={place.credit}></a> {place.creditName} on <a href={place.source}></a> {place.sourceName}</p>
+                </div> 
+                <h3>Rating</h3>
+                <p>Not Rated</p>
+                <h3>Description</h3>
+                <p>{ place.city }</p>
+                <footer>Comments</footer>
+                <a href={`/places/${place.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
             </main>
         </Def>
     )
