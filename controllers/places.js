aa76370207bc483d14598087e0c80c50517a8f03
MalMWare <malkelleymartin@gmail.com>
2023-02-19 21:11:57 -0800

    added mongo and mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,88 +1,98 @@
 const router = require('express').Router()
 const places = require("../models/places.js")
+const db = require('../models')
 
 router.get('/', (req, res) => {
-    res.render('places/index', { places })
+    db.Place.find()
+    .then((places) => {
+        res.render('places/index', { places })
+    })
+    .catch( err => {
+        console.log(err)
+        res.render('error404')
+    })
 })
 
-//create
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-        req.body.pic = "https://placekitten.com/400/500"
-    }
-    if (!req.body.city) {
-        req.body.city = 'Anywhere'
-    }
-    if (!req.body.state) {
-        req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-})
-
-//new
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
-
-//show
-router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      res.render('places/show', { place: places[id], id: req.params.id})
-    }
-})
-
-
-//delete
-router.delete("/:id", (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        places.splice(id, 1)
-        //res.send('STUB DELETE places/:id')
+    db.Place.create(req.body)
+    .then(() => {
         res.redirect('/places')
-    }
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
-//put
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
 router.put('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        if (!req.body.pic) {
-            req.body.pic = 'http://placekitten.com/400/400'
-        }
-        if (!req.body.city) {
-            req.body.city = 'Anytown'
-        }
-        if (!req.body.state) {
-            req.body.state = 'USA'
-        }
-        places[id] =req.body
-        res.redirect(`/places/${id}`)
-    }
+  res.send('PUT /places/:id stub')
 })
 
-//edit
-// router.get('/:id/edit', (req, res) => {
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+
+// //create
+// router.post('/', (req, res) => {
+//     if (!req.body.pic) {
+//         req.body.pic = "https://placekitten.com/400/500"
+//     }
+//     if (!req.body.city) {
+//         req.body.city = 'Anywhere'
+//     }
+//     if (!req.body.state) {
+//         req.body.state = 'USA'
+//     }
+//     places.push(req.body)
+//     res.redirect('/places')
+// })
+
+// //new
+// router.get('/new', (req, res) => {
+//     res.render('places/new')
+// })
+
+
+// //show
+// router.get('/:id', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//       res.render('error404')
+//     }
+//     else if (!places[id]) {
+//       res.render('error404')
+//     }
+//     else {
+//       res.render('places/show', { place: places[id], id: req.params.id})
+//     }
+// })
+
+
+// //delete
+// router.delete("/:id", (req, res) => {
 //     let id = Number(req.params.id)
 //     if (isNaN(id)) {
 //         res.render('error404')
@@ -91,19 +101,59 @@ router.put('/:id', (req, res) => {
 //         res.render('error404')
 //     }
 //     else {
-//         res.redirect('places/edit', { place: places[id] })
+//         places.splice(id, 1)
+//         //res.send('STUB DELETE places/:id')
+//         res.redirect('/places')
 //     }
 // })
-  
 
-// EDIT
-router.get('/:id/edit', (req, res) => {
-    res.render('places/edit', { place: places[req.params.id], id: req.params.id })
-})
-// router.get('/:id', (req, res) => {
-//     (places[req.params.id])
-//       ? res.render('places/show', { place: places[req.params.id], id: req.params.id })
-//       : res.status(404).render('Error404')
+// //put
+// router.put('/:id', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//         res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     else {
+//         if (!req.body.pic) {
+//             req.body.pic = 'http://placekitten.com/400/400'
+//         }
+//         if (!req.body.city) {
+//             req.body.city = 'Anytown'
+//         }
+//         if (!req.body.state) {
+//             req.body.state = 'USA'
+//         }
+//         places[id] =req.body
+//         res.redirect(`/places/${id}`)
+//     }
 // })
 
-module.exports = router
+// //edit
+// // router.get('/:id/edit', (req, res) => {
+// //     let id = Number(req.params.id)
+// //     if (isNaN(id)) {
+// //         res.render('error404')
+// //     }
+// //     else if (!places[id]) {
+// //         res.render('error404')
+// //     }
+// //     else {
+// //         res.redirect('places/edit', { place: places[id] })
+// //     }
+// // })
+  
+
+// // EDIT
+// router.get('/:id/edit', (req, res) => {
+//     res.render('places/edit', { place: places[req.params.id], id: req.params.id })
+// })
+// // router.get('/:id', (req, res) => {
+// //     (places[req.params.id])
+// //       ? res.render('places/show', { place: places[req.params.id], id: req.params.id })
+// //       : res.status(404).render('Error404')
+// // })
+
+// module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,23 +1,36 @@
-module.exports = [{
-    name: 'Gorilla Sushi',
-    city: 'Las Vegas',
-    state: 'NV',
-    cuisines: 'Sushi, Japanese Food, Buffet/AYCE',
-    pic: '/images/smallSushiDinner.jpg',
-    class: 'sushi-dinner',
-    credit: "https://unsplash.com/de/@metzograf?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-    source: "https://unsplash.com/photos/12IsagncQ7o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-    creditName: 'Florian Metzner',
-    sourceName: 'Unsplash'
-}, {
-    name: 'Black Tap',
-    city: 'Las Vegas',
-    state: 'NV',
-    cuisines: 'American, Burgers, Shakes',
-    pic: '/images/smallIcecream.jpg',
-    class: 'icecream-shake',
-    credit: "https://unsplash.com/@m_usm04?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-    source: "https://unsplash.com/photos/OSpvTwWvjmU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
-    creditName: 'Muhammad Usman',
-    sourceName: 'Unsplash'
-}]
+// module.exports = [{
+//     name: 'Gorilla Sushi',
+//     city: 'Las Vegas',
+//     state: 'NV',
+//     cuisines: 'Sushi, Japanese Food, Buffet/AYCE',
+//     pic: '/images/smallSushiDinner.jpg',
+//     class: 'sushi-dinner',
+//     credit: "https://unsplash.com/de/@metzograf?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+//     source: "https://unsplash.com/photos/12IsagncQ7o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+//     creditName: 'Florian Metzner',
+//     sourceName: 'Unsplash'
+// }, {
+//     name: 'Black Tap',
+//     city: 'Las Vegas',
+//     state: 'NV',
+//     cuisines: 'American, Burgers, Shakes',
+//     pic: '/images/smallIcecream.jpg',
+//     class: 'icecream-shake',
+//     credit: "https://unsplash.com/@m_usm04?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+//     source: "https://unsplash.com/photos/OSpvTwWvjmU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+//     creditName: 'Muhammad Usman',
+//     sourceName: 'Unsplash'
+// }]
+
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    pic: String,
+    cuisines: { type: String, required: true},
+    city: { type: String, default: 'Anytown'},
+    state: { type: String, defsult: "USA"},
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
