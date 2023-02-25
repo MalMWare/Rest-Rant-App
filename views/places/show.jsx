55256430a73664211e63b468076a8ed52a016ec3
MalMWare <malkelleymartin@gmail.com>
2023-02-24 23:05:01 -0800

    created comment form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,49 @@ const React = require('react')
 const Def = require('../default')
 
 function show ({place, id}) {
+    function commentForm() {
+        return (
+          <form method='POST' action={`/places/${place.id}/comment`} className='comment-form'>
+          <legend className='comment-legend'><h4>Add A Comment</h4></legend>
+          <div className='comment-auth'>
+              <label htmlFor='author'>Author: </label>
+              <input
+                  type='text'
+                  name='author'
+              />
+          </div>
+          <div className='comment-stars'>
+              <label htmlFor="stars">Rating: </label>
+              <input
+                  type="number"
+                  name='stars'
+                  step='0.5'
+                  min='0.5'
+                  max='5'
+                  defaultValue='0.5'
+              />
+          </div>
+          <div className='comment-rant'>
+              <label htmlFor="rant">Rant: </label>
+              <input
+                  type="checkbox"
+                  name='rant'
+                  value='rant'
+              />
+          </div>
+          <div className='comment-content'>
+              <label htmlFor="content">Content: </label>
+              <textarea
+                  name="content"
+                  rows='5'
+              ></textarea>
+          </div>
+          <div className='comment-submit'>
+              <button type='submit' className='btn btn-primary'>Submit</button>
+          </div>
+      </form>
+        )
+    }
     let comments = (
         <h3 className='inactive'>
             No comments yet!
@@ -38,7 +81,7 @@ function show ({place, id}) {
                 <p>{ place.showEstablished() }</p>
                 <p>Serving { place.cuisines }</p>
                 <h2>Comments</h2>
-                {comments}
+                {comments} {commentForm()}
                 <a href={`/places/${place.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
