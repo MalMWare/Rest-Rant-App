e8f2aa9fe3aae8125d90c89b89b77c22e685cf39
MalMWare <malkelleymartin@gmail.com>
2023-02-27 20:03:07 -0800

    stars instead of number rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -54,19 +54,22 @@ function show ({place, id}) {
         <h3 className="inactive">
           Not yet rated
         </h3>
-      )
+    )
       if (place.comments.length) {
         let sumRatings = place.comments.reduce((tot, c) => {
           return tot + c.stars
         }, 0)
-        let averageRating = sumRatings / place.comments.length
+        let averageRating = Math.round(sumRatings / place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+          stars += '★'
+        }
         rating = (
-          <h3>
-            {Math.round(averageRating)} stars
-          </h3>
+            <h3>
+                {stars} stars
+            </h3>
         )
-      }
-      
+    }
     if (place.comments.length) {
         comments = place.comments.map(c => {
             return (
